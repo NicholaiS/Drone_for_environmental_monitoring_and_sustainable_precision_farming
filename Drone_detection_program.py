620a19932b856619ff65f6eed1febd47a8c1f1a3
NicholaiS <nicholai.h.skov@hotmail.com>
2023-08-13 15:24:51 +0200

    Update

--- Drone_detection_program.py
+++ Drone_detection_program.py
@@ -1,39 +1,105 @@
+from pickle import FALSE
 import cv2
 import numpy as np
+import random
 import matplotlib.pyplot as plt
 
-indir = 'C:\\Users\\Nicho\\OneDrive\\Dokumenter\\VSC Workspace\\Visuals for drone (Summer course)\\Drone pics\\'
-outdir = 'C:\\Users\\Nicho\\OneDrive\\Dokumenter\\VSC Workspace\\Visuals for drone (Summer course)\\Drone pics out\\'
+loadColor = False
 
 # Reading the image and resizing:
-img = cv2.resize(cv2.imread(indir + "20230811_115843151_iOS.jpg"), (1080, 720))
+img = cv2.resize(cv2.imread("Drone pics\\3.jpg"), (1080, 720))
 height, width = img.shape[:2]
-#img = cv2.resize(img, (1080, 720))
 pixels = np.reshape(img, (-1, 3))
 
+def find_avg_and_cov():
+    # Finding the mean of the color and the covariance:
+    img_annotated = cv2.resize(cv2.imread('Drone pics\\7_rod.jpg'), (1080, 720))
+    mask = cv2.inRange(img_annotated, (30, 45, 230), (40, 55, 235))
+    mask_pixels = np.reshape(mask, (-1))
+    cv2.imshow("Mask Image", mask)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    # Determine mean value, standard deviations and covariance matrix
+    # for the annotated pixels.
+    # Using cv2 to calculate mean and standard deviations
+    mean, std = cv2.meanStdDev(img, mask = mask)
+
+    pixels = np.reshape(img, (-1, 3))
+    mask_pixels = np.reshape(mask, (-1))
+    annot_pix_values = pixels[mask_pixels == 255, ]
+    avg = np.average(annot_pix_values, axis=0)
+    cov = np.cov(annot_pix_values.transpose()) 
+
+    print("Mean color values of the annotated pixels")
+    print(avg)
+    print("Covariance matrix of color values of the annotated pixels")
+    print(cov)
+
+    # Save avg and cov to a text file
+    np.savetxt("avg_cov.txt", np.concatenate((avg, cov.flatten())))
+
 # ------------------------------------------- Color segmentation ---------------------------------------------
-# Farven du vil ramme i RGB: (157, 95, 38)
-img_annotated = cv2.resize(cv2.imread(indir + '20230811_115843151_iOS_rod.jpg'), (1080, 720))
-mask = cv2.inRange(img_annotated, (245, 0, 0), (256, 10, 10))
-mask_pixels = np.reshape(mask, (-1))
-cv2.imshow("Image", mask)
+
+if loadColor:
+   find_avg_and_cov()
+
+# Using Mahalanobis distance using the mean and covariance:
+# Load avg and cov from the text file
+loaded_data = np.loadtxt("avg_cov.txt")
+loaded_avg = loaded_data[:3]
+loaded_cov = loaded_data[3:].reshape(3, 3)
+
+reference_color = loaded_avg # Given in BGR
+covariance_matrix = loaded_cov
+
+# Calculate the euclidean distance to the reference_color annotated color.
+shape = pixels.shape
+diff = pixels - np.repeat([reference_color], shape[0], axis=0)
+inv_cov = np.linalg.inv(covariance_matrix)
+moddotproduct = diff * (diff @ inv_cov)
+mahalanobis_dist = np.sum(moddotproduct, axis=1)
+mahalanobis_distance_image = np.reshape(mahalanobis_dist, (img.shape[0], img.shape[1]))
+
+# Scale the distance image and export it.
+mahalanobis_distance_image = 255 * mahalanobis_distance_image / np.max(mahalanobis_distance_image)
+cv2.imshow("Mahalanobis Distance Image", mahalanobis_distance_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imwrite("Pics out/mahalanobis_dist_image.jpg", mahalanobis_distance_image)
+
+
+# ------------------------------------------- Feature Extraction ---------------------------------------------
+imgfe = cv2.imread("Pics out/mahalanobis_dist_image.jpg")
+imgfeGray = cv2.cvtColor(imgfe, cv2.COLOR_BGR2GRAY)
+tLower = 50
+tUpper = 250
+imgCanny = cv2.Canny(imgfeGray, tLower, tUpper)
+
+contours, hierarchy = cv2.findContours(imgCanny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+cv2.drawContours(imgfe, contours, -1, (0, 255, 0), 2)
+
+cv2.imshow("Contour Image", imgfe)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-# Determine mean value, standard deviations and covariance matrix
-# for the annotated pixels.
-# Using cv2 to calculate mean and standard deviations
-mean, std = cv2.meanStdDev(img, mask = mask)
+for contour in contours:
+    # Calculate moments for each contour
+    moments = cv2.moments(contour)
 
-pixels = np.reshape(img, (-1, 3))
-mask_pixels = np.reshape(mask, (-1))
-annot_pix_values = pixels[mask_pixels == 255, ]
-avg = np.average(annot_pix_values, axis=0)
-cov = np.cov(annot_pix_values.transpose()) 
-
-print("Mean color values of the annotated pixels")
-print(avg)
-print("Covariance matrix of color values of the annotated pixels")
-print(cov)
-
-# ------------------------------------------- Feature Extraction ---------------------------------------------
+    drawing = 0 * imgfeGray
+    # Calculate centroid
+    if moments['m00'] != 0:  # Avoid division by zero
+        cX = int(moments['m10'] / moments['m00'])
+        cY = int(moments['m01'] / moments['m00'])
+        centroid = (cX, cY)
+        
+        # Draw centroid on the image
+        cv2.circle(imgfe, centroid, 5, (0, 0, 255), -1)
+    
+    # Calculate other moments and properties if needed
+    # For example: area, orientation, eccentricity, etc.
+    
+cv2.imshow("Contour Image with Centroids", imgfe)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
